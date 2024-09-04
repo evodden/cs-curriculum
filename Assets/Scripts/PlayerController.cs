e89ff87234ddba828b96072307762653bf31052f
evodden <evodden4322@student.pps.net>
2024-09-04 15:22:33 -0700

    added up down movement and left right movement to the main player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool overworld; 
+    float xspeed;
+    float xdirection;
+    float xvector;
 
+    float yspeed;
+    float ydirection;
+    float yvector;
+    Rigidbody2D rb;
+    public bool overworld; 
+    
     private void Start()
     {
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
-        
-        
+
+        xspeed = 5;
+        yspeed = 5;
         if (overworld)
         {
             GetComponent<Rigidbody2D>().gravityScale = 0f;
@@ -25,7 +34,13 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        
+        xdirection = Input.GetAxis("Horizontal");
+        xvector = xspeed * xdirection * Time.deltaTime;
+        transform.Translate(xvector, 0, 0);
+
+        ydirection = Input.GetAxis("Vertical");
+        yvector = yspeed * ydirection * Time.deltaTime;
+        transform.Translate(0, yvector, 0);
     }
     
     //for organization, put other built-in Unity functions here
