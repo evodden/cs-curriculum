76d6a0be61e09ad293060f22e69656b36a3a8aed
evodden <evodden4322@student.pps.net>
2024-10-08 15:29:19 -0700

    worked on projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public Vector3 target;
-
+    private Vector3 direction;
     public float projectileSpeed = 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
